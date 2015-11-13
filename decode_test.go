e2dc1223c2c1991dd5aed3163761cacef0ba295b
Robert Hülle <robert.hulle@gmail.com>
2015-11-13 23:36:01 +0100

    remove external test dependency

--- decode_test.go
+++ decode_test.go
@@ -2,7 +2,6 @@ package imagefile
 
 import (
 	"bytes"
-	"github.com/stretchr/testify/assert"
 	"image"
 	"io"
 	"testing"
@@ -32,30 +31,40 @@ func TestDecodeImage(t *testing.T) {
 	for _, test := range decodeTests {
 		r := bytes.NewBuffer(test.input)
 		img, err := Decode(r)
-		if !assert.Nil(t, err, "test %s: err not nil: %v", test.descr, err) {
+		if err != nil {
+			t.Errorf("%s: err not nil: %v", test.descr, err)
 			continue
 		}
-		if !assert.IsType(t, (*image.NRGBA)(nil), img, "test %s", test.descr) {
+		nrgba, ok := img.(*image.NRGBA)
+		if !ok {
+			t.Errorf("%s: wrong image type")
 			continue
 		}
-		nrgba := img.(*image.NRGBA)
-		assert.Equal(t, test.w, img.Bounds().Dx(), "test %s: different width", test.descr)
-		assert.Equal(t, test.h, img.Bounds().Dy(), "test %s: different height", test.descr)
-		assert.Equal(t, test.img, nrgba.Pix, "test %s: image data differs", test.descr)
+		if dx, dy := img.Bounds().Dx(), img.Bounds().Dy(); dx != test.w || dy != test.h {
+			t.Errorf("%s: wrong image size: %d x %d | expected: %d x %d",
+				test.descr, dx, dy, test.w, test.h)
+		}
+		if 0 != bytes.Compare(test.img, nrgba.Pix) {
+			t.Errorf("%s: image data differs", test.descr)
+		}
 	}
 }
 
 func TestDecodeImageBadHeader(t *testing.T) {
 	r := bytes.NewBuffer([]byte("imagefile\000\000\000\000\000\000\000"))
 	img, err := Decode(r)
-	assert.NotNil(t, err, "err is nil")
-	assert.Equal(t, io.ErrUnexpectedEOF, err, "err is wrong")
-	assert.Nil(t, img, "img is not nil")
+	if err != io.ErrUnexpectedEOF {
+		t.Errorf("err: %#v | expected: %#v", err, io.ErrUnexpectedEOF)
+	}
+	if img != nil {
+		t.Errorf("img is not nil")
+	}
 }
 
 func TestDecodeImageBad(t *testing.T) {
 	r := bytes.NewBuffer([]byte("imagefile\000\000\000\001\000\000\000\001rgb"))
 	_, err := Decode(r)
-	assert.NotNil(t, err, "err is nil")
-	assert.Equal(t, io.ErrUnexpectedEOF, err, "err is wrong")
+	if err != io.ErrUnexpectedEOF {
+		t.Errorf("err: %#v | expected: %#v", err, io.ErrUnexpectedEOF)
+	}
 }
--- encode_test.go
+++ encode_test.go
@@ -2,7 +2,6 @@ package imagefile
 
 import (
 	"bytes"
-	"github.com/stretchr/testify/assert"
 	"image"
 	"image/color"
 	"testing"
@@ -12,9 +11,12 @@ func TestEncodeEmptyImage(t *testing.T) {
 	img := image.NewRGBA(image.Rect(0, 0, 0, 0))
 	w := new(bytes.Buffer)
 	err := Encode(w, img)
-	assert.Nil(t, err, "err is not nil: %v", err)
-	assert.Equal(t, []byte("imagefile\000\000\000\000\000\000\000\000"),
-		w.Bytes(), "encoded image differs")
+	if err != nil {
+		t.Errorf("err is not nil: %v", err)
+	}
+	if 0 != bytes.Compare([]byte("imagefile\000\000\000\000\000\000\000\000"), w.Bytes()) {
+		t.Errorf("encoded image differs")
+	}
 }
 
 func TestEncodeSmallImage(t *testing.T) {
@@ -22,9 +24,12 @@ func TestEncodeSmallImage(t *testing.T) {
 	img.Pix = []byte("abcd")
 	w := new(bytes.Buffer)
 	err := Encode(w, img)
-	assert.Nil(t, err, "err is not nil: %v", err)
-	assert.Equal(t, []byte("imagefile\000\000\000\001\000\000\000\001abcd"),
-		w.Bytes(), "encoded image differs")
+	if err != nil {
+		t.Errorf("err is not nil: %v", err)
+	}
+	if 0 != bytes.Compare([]byte("imagefile\000\000\000\001\000\000\000\001abcd"), w.Bytes()) {
+		t.Errorf("encoded image differs")
+	}
 }
 
 func TestEncodeGrayImage(t *testing.T) {
@@ -32,23 +37,12 @@ func TestEncodeGrayImage(t *testing.T) {
 	img.Set(0, 0, color.White)
 	w := new(bytes.Buffer)
 	err := Encode(w, img)
-	assert.Nil(t, err, "err is not nil: %v", err)
-	assert.Equal(t, []byte("imagefile\000\000\000\001\000\000\000\001\xff\xff\xff\xff"),
-		w.Bytes(), "encoded image differs")
-}
-
-func TestEncodePremultiplied(t *testing.T) {
-	img := image.NewRGBA(image.Rect(0, 0, 1, 1))
-	img.Set(0, 0, color.NRGBA{0x33, 0x44, 0x55, 0x66})
-	w := new(bytes.Buffer)
-	err := Encode(w, img)
-	assert.Nil(t, err, "err is not nil: %v", err)
-	assert.Equal(t, []byte("imagefile\000\000\000\001\000\000\000\001"),
-		w.Bytes()[:17], "encoded image differs")
-	assert.True(t, testAlmostEqual(2, 0x33, int(w.Bytes()[17])), "encoded image differs too much")
-	assert.True(t, testAlmostEqual(2, 0x44, int(w.Bytes()[18])), "encoded image differs too much")
-	assert.True(t, testAlmostEqual(2, 0x55, int(w.Bytes()[19])), "encoded image differs too much")
-	assert.True(t, testAlmostEqual(2, 0x66, int(w.Bytes()[20])), "encoded image differs too much")
+	if err != nil {
+		t.Errorf("err is not nil: %v", err)
+	}
+	if 0 != bytes.Compare([]byte("imagefile\000\000\000\001\000\000\000\001\xff\xff\xff\xff"), w.Bytes()) {
+		t.Errorf("encoded image differs")
+	}
 }
 
 func TestEncodeSubImage(t *testing.T) {
@@ -61,17 +55,13 @@ func TestEncodeSubImage(t *testing.T) {
 	}
 	w := new(bytes.Buffer)
 	err := Encode(w, img.SubImage(image.Rect(1, 1, 3, 3)))
-	assert.Nil(t, err, "err is not nil")
-	assert.Equal(t, []byte("imagefile\000\000\000\002\000\000\000\002"+
+	if err != nil {
+		t.Errorf("err is not nil: %v", err)
+	}
+	if 0 != bytes.Compare([]byte("imagefile\000\000\000\002\000\000\000\002"+
 		"\x14\x15\x16\x17\x18\x19\x1a\x1b"+
 		"\x24\x25\x26\x27\x28\x29\x2a\x2b"),
-		w.Bytes(), "encoded image differs")
-}
-
-func testAlmostEqual(diff, expected, got int) bool {
-	adiff := expected - got
-	if adiff < 0 {
-		adiff = -adiff
+		w.Bytes()) {
+		t.Errorf("encoded image differs")
 	}
-	return diff >= adiff
 }
